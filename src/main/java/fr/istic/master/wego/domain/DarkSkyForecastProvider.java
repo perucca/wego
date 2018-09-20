dcd7c9c599a4a011a633ffda7791de5a78ad8254
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2018-09-20 15:51:31 +0200

    Weather API key

---
+++ src/main/java/fr/istic/master/wego/domain/DarkSkyForecastProvider.java
@@ -0,0 +1,5 @@
+package fr.istic.master.wego.domain;
+
+public class DarkSkyForecastProvider {
+	private static String key = "79d32c95d548b265f6607278590c5437";
+}
---
+++ src/main/java/fr/istic/master/wego/domain/OpenWeatherMapForecastProvider.java
@@ -0,0 +1,5 @@
+package fr.istic.master.wego.domain;
+
+public class OpenWeatherMapForecastProvider {
+	private static String key = "d57ff8c8ad6d033dbf8cf1b6f278dd1e";
+}
