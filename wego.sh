ee473e0219a72aad18c3d7e0d93d3eb08954deef
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2018-09-20 16:24:07 +0200

    le démarrage de l'application sous docker est fonctionnel

--- wego.sh
+++ wego.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#sudo docker build -t wego .
+sudo docker build -t wego .
 sudo docker network rm 'wego-network'
 sudo docker network create 'wego-network'
 
+sudo docker kill database
 sudo docker rm database
-sudo docker run --network='wego-network' --name database -v /home/michel/mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wego -d mysql:5.7.23
-#sudo docker run --publish 8080:8080 --network='wego-network' wego:latest
+sudo docker run --network='wego-network' --name database \
+        -v '/home/michel/mysql-data':'/var/lib/mysql' \
+        -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wego \
+        --publish=3306:3306 -d mysql:5.7.23
+sudo docker run --publish 8080:8080 --network='wego-network' wego:latest
