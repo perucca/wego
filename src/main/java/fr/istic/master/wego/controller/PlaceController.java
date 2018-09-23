7b5c5cba6523ba96a40fdf306894a5dcce4d2276
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-09-23 23:38:18 +0200

    Transformed the dto classes into simple POJO, created the static methods to operate all the dto transformations, added service classes and moved the code from controllers to the services

--- src/main/java/fr/istic/master/wego/controller/PlaceController.java
+++ src/main/java/fr/istic/master/wego/controller/PlaceController.java
@@ -1,9 +1,11 @@
 package fr.istic.master.wego.controller;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import fr.istic.master.wego.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,38 +27,27 @@ import fr.istic.master.wego.model.Place;
 public class PlaceController {
 
 	@Autowired
-	private PlaceDao placeDao;
-	
-	//Methodes pour get une liste de place
+	private PlaceService placeService;
+
+	@GetMapping("")
+	public Collection<PlaceDto> getAllPlaces() {
+		return placeService.getAllPlaces();
+
+	}
 	
 	@GetMapping("/bypostcode/{postcode}")
-	public Set<PlaceDto> getAllPlacesByPostCode(@PathVariable("postcode") String postcode) {
-		return placeDao.findAllByPostCode(postcode).stream().map(p->new PlaceDto(p.getId(), p.getName(), p.getPostCode())).collect(Collectors.toSet());
+	public Collection<PlaceDto> getAllPlacesByPostCode(@PathVariable("postcode") String postcode) {
+		return placeService.getAllPlacesByPostCode(postcode);
 	}
 
 	@GetMapping("/byname/{name}")
-	public Set<PlaceDto> getAllPlacesByName(@PathVariable("name") String name) {
-		return placeDao.findAllByName(name).stream().map(p->new PlaceDto(p.getId(), p.getName(), p.getPostCode())).collect(Collectors.toSet());
+	public Collection<PlaceDto> getAllPlacesByName(@PathVariable("name") String name) {
+		return placeService.getAllPlacesByName(name);
 	}
-	
-	@GetMapping("")
-	public List<PlaceDto> getAllPlaces() {
-		return placeDao.findAll().stream().map(p->new PlaceDto(p.getId(), p.getName(), p.getPostCode())).collect(Collectors.toList());
-	}
-
-	// Methode pour get une ville by ID
 	
 	@GetMapping("/{id}")
 	public PlaceDto getPlaceById(@PathVariable("id") Long id) {
-		Place p = placeDao.getOne(id);
-		return new PlaceDto(p.getId(), p.getName(), p.getPostCode());			
+		return placeService.getPlacesById(id);
 	}
-	
-
-	
-//	@GetMapping("/places")
-//	public Page<Place> getAllPlaces(Pageable pageable) {
-//		return placeDao.findAll(pageable);
-//	}
 
 }
--- src/main/java/fr/istic/master/wego/controller/UserPlaceController.java
+++ src/main/java/fr/istic/master/wego/controller/UserPlaceController.java
@@ -1,66 +1,64 @@
-package fr.istic.master.wego.controller;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import fr.istic.master.wego.dao.PlaceDao;
-import fr.istic.master.wego.dao.UserDao;
-import fr.istic.master.wego.dao.UserPlaceDao;
-import fr.istic.master.wego.dto.PlaceDto;
-import fr.istic.master.wego.dto.user.UserPlaceDtoCreate;
-import fr.istic.master.wego.dto.user.UserPlaceDtoDisplay;
-import fr.istic.master.wego.model.UserPlace;
-
-/**
- * @author amontuwy REST services for UserPlace Management To be done
- */
-@RestController
-@RequestMapping("/userplaces")
-public class UserPlaceController {
-
-	@Autowired
-	private UserPlaceDao userPlaceDao;
-	@Autowired
-	private UserDao userDao;
-	@Autowired
-	private PlaceDao placeDao;
-	
-	//Create a userplace
-	@PostMapping("")
-	public Optional<UserPlace> createUserPlace(@RequestBody @Valid UserPlaceDtoCreate updto) {
-		UserPlace up = new UserPlace();
-		up.setUser(userDao.getOne(updto.getUserid()));
-		up.setPlace(placeDao.getOne(updto.getPlaceid()));
-		up.setPreferenceOrder(updto.getPreferenceOrder());
-		UserPlace u = userPlaceDao.save(up);
-		return userPlaceDao.findById(u.getId());
-	}
-	
-	//Read all userplaces
-	@GetMapping("")
-	Collection <UserPlaceDtoDisplay> getAllUserPlaces(){
-		Set<UserPlaceDtoDisplay> upd = new HashSet<UserPlaceDtoDisplay>(); //on cree un collection vide à retourner ensuite
-		List<UserPlace> updao = userPlaceDao.findAll(); //on recupere les user place en base
-		for (UserPlace p : updao) { //pour chaque userplace on crée le userplaceDtoDisplay correspondant
-			UserPlaceDtoDisplay tmp = new UserPlaceDtoDisplay();
-			PlaceDto pdto = new PlaceDto(p.getPlace().getId(), p.getPlace().getName(), p.getPlace().getPostCode());
-			tmp.setPlace(pdto);
-			tmp.setPreferenceOrder(p.getPreferenceOrder());
-			upd.add(tmp);
-		}
-		return upd;
-	}
-
-}
+//package fr.istic.master.wego.controller;
+//
+//import java.util.Collection;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.Set;
+//
+//import javax.validation.Valid;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import fr.istic.master.wego.dao.PlaceDao;
+//import fr.istic.master.wego.dao.UserDao;
+//import fr.istic.master.wego.dao.UserPlaceDao;
+//import fr.istic.master.wego.dto.PlaceDto;
+//import fr.istic.master.wego.model.UserPlace;
+//
+///**
+// * @author amontuwy REST services for UserPlace Management To be done
+// */
+//@RestController
+//@RequestMapping("/userplaces")
+//public class UserPlaceController {
+//
+//	@Autowired
+//	private UserPlaceDao userPlaceDao;
+//	@Autowired
+//	private UserDao userDao;
+//	@Autowired
+//	private PlaceDao placeDao;
+//
+//	//Create a userplace
+//	@PostMapping("")
+//	public Optional<UserPlace> createUserPlace(@RequestBody @Valid UserPlaceDtoCreate updto) {
+//		UserPlace up = new UserPlace();
+//		up.setUser(userDao.getOne(updto.getUserid()));
+//		up.setPlace(placeDao.getOne(updto.getPlaceid()));
+//		up.setPreferenceOrder(updto.getPreferenceOrder());
+//		UserPlace u = userPlaceDao.save(up);
+//		return userPlaceDao.findById(u.getId());
+//	}
+//
+//	//Read all userplaces
+//	@GetMapping("")
+//	Collection <UserPlaceDtoDisplay> getAllUserPlaces(){
+//		Set<UserPlaceDtoDisplay> upd = new HashSet<UserPlaceDtoDisplay>(); //on cree un collection vide à retourner ensuite
+//		List<UserPlace> updao = userPlaceDao.findAll(); //on recupere les user place en base
+//		for (UserPlace p : updao) { //pour chaque userplace on crée le userplaceDtoDisplay correspondant
+//			UserPlaceDtoDisplay tmp = new UserPlaceDtoDisplay();
+//			PlaceDto pdto = new PlaceDto(p.getPlace().getId(), p.getPlace().getName(), p.getPlace().getPostCode());
+//			tmp.setPlace(pdto);
+//			tmp.setPreferenceOrder(p.getPreferenceOrder());
+//			upd.add(tmp);
+//		}
+//		return upd;
+//	}
+//
+//}
