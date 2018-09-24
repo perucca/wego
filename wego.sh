b22c3aff766da07f1e470b48518d89fc36d2212d
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2018-09-24 22:54:56 +0200

    Utilisation de script bash et mise en place de docker compose

--- wego.sh
+++ wego.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Nettoyage
+kill_container() {
+    id=$(docker run $1)
+    if docker top $id &>/dev/null
+    then
+      sudo docker stop $1 
+      sudo docker network disconnect 'wego-network' $1  
+    fi
+}
+
+
+kill_container wego
+kill_container database
+sudo docker network rm 'wego-network' 2>/dev/null
+
+# Construction des images
+
 sudo docker build -t wego .
-sudo docker network rm 'wego-network'
+
+# Démarrage du réseau et des applications
 sudo docker network create 'wego-network'
 
-sudo docker kill database
-sudo docker rm database
 sudo docker run --network='wego-network' --name database \
         -v '/home/michel/mysql-data':'/var/lib/mysql' \
         -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wego \
