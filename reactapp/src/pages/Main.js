e3e994f37413f2937f53f17e4d6b8b518df050b2
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-10-11 23:20:42 +0200

    added all the secured pages in a private route

--- reactapp/src/pages/Main.js
+++ reactapp/src/pages/Main.js
@@ -9,10 +9,10 @@ const Main = (props) => (
         <Switch>
             <Route exact path="/" component={LoginPage} />
             <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/profile" component={ProfilePage} />
-            <Route exact path="/mysports" component={MySportsPage} />
             <Route exact path="/signin" component={SignInPage} />
-            <PrivateRoute exact path="/home" component={HomePage} name="coco"/>
+            <PrivateRoute exact path="/profile" component={ProfilePage} />
+            <PrivateRoute exact path="/mysports" component={MySportsPage} />
+            <PrivateRoute exact path="/home" component={HomePage}/>
             <PrivateRoute exact path="/myplaces" component={MyPlacesPage}/>
         </Switch>
     </main>
