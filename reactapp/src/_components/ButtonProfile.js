cab4b51ae0e9b3eb9eb34a4727917af13f1f5974
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-10-11 23:02:48 +0200

    fixed className

--- reactapp/src/_components/ButtonProfile.js
+++ reactapp/src/_components/ButtonProfile.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ButtonProfile = (props) => (
     <div className="col-12">
-            <a href={props.type} class="btn btn-info btn-lg my-1" role="button">{props.name}</a>
+            <a href={props.type} className="btn btn-info btn-lg my-1" role="button">{props.name}</a>
         </div>
 
 
