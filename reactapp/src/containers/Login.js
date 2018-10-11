9ddb8edd5472028b8594ed36e7afb47feef4a227
Perucca Michel <michel.perucca@etudiant.univ-rennes1.fr>
2018-10-11 21:17:30 +0200

    Mise en place proxy et léger refacto de la requête

--- reactapp/src/containers/Login.js
+++ reactapp/src/containers/Login.js
@@ -15,52 +15,44 @@ class Login extends React.Component {
 
     onClick = (event) => {
         event.preventDefault();
-        Axios({
-            url:'http://localhost:8080/login',  
-            method: 'post',
-            data:{}, 
-            auth: {
-                    username: this.state.username,
-                    password: this.state.password
-            }
-        })
+        Axios.post('/login',{}, {auth: { username: this.state.username,
+                                         password: this.state.password }}
+        )
         .then(function (response) {
+            //Trace de la réponse
             console.log(response);
 
-            Axios({
-                url:'http://localhost:8080/places',  
-                method: 'get',
-                data:{ }, 
-           
-            }).then(function (response) {
-                console.log(response);
-            }).catch(function (error){console.log(error)})
+            //On code ici le routing sur HOME
+
         })
         .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error);
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-              } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error);
-                console.log(error.request);
-              } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-              }
-              console.log(error.config);
+            //on trace l'erreur au maximum
+            this.logError(error);
+
+            //Gérer le code 401 ici pour retourner un message d'erreur
         });
     }
 
     onChange = (event) => {event.persist();
         console.log(event.target.name); 
         this.setState({[event.target.name]: event.target.value})}
+
+    logError(error) {
+        if (error.response) {
+            console.log(error);
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        }
+        else if (error.request) {
+            console.log(error);
+            console.log(error.request);
+        }
+        else {
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+    }
 
     render() { return (
        <form>
