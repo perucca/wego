b994a9637754038e13c1d9afb98d736136b82e57
Screm Dorian <dorian.screm@etudiant.univ-rennes1.fr>
2018-10-15 16:16:22 +0200

    added some fake data for user-sport

--- src/main/java/fr/istic/master/wego/DataBaseLoader.java
+++ src/main/java/fr/istic/master/wego/DataBaseLoader.java
@@ -1,16 +1,9 @@
 package fr.istic.master.wego;
 
-import fr.istic.master.wego.dao.SportDao;
+import fr.istic.master.wego.dao.*;
+import fr.istic.master.wego.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import fr.istic.master.wego.dao.PlaceDao;
-import fr.istic.master.wego.dao.UserDao;
-import fr.istic.master.wego.dao.UserPlaceDao;
-import fr.istic.master.wego.model.Place;
-import fr.istic.master.wego.model.Sport;
-import fr.istic.master.wego.model.User;
-import fr.istic.master.wego.model.UserPlace;
 
 @Component
 public class DataBaseLoader {
@@ -23,22 +16,30 @@ public class DataBaseLoader {
 	UserDao userdao;
 	@Autowired
 	UserPlaceDao upd;
+	@Autowired
+	UserSportDao usd;
 
 	public void loadData() {
 
 		// Declaration de plusieurs lieux de test
 		Sport kayak = new Sport();
 		kayak.setSportName("kayak");
+		kayak.setType(TypeSport.EAU);
 		Sport foot = new Sport();
 		foot.setSportName("football");
+		foot.setType(TypeSport.TERRE);
 		Sport golf = new Sport();
 		golf.setSportName("golf");
+		golf.setType(TypeSport.TERRE);
 		Sport voile = new Sport();
 		voile.setSportName("voile");
+		voile.setType(TypeSport.AIR);
 		Sport tennis = new Sport();
 		tennis.setSportName("tennis");
+		tennis.setType(TypeSport.TERRE);
 		Sport ski = new Sport();
 		ski.setSportName("ski");
+		ski.setType(TypeSport.NEIGE);
 
 		// déclaration de plusieurs user de test
 		User p1 = new User();
@@ -187,6 +188,38 @@ public class DataBaseLoader {
 		up10.setUser(p4);
 		up10.setPreferenceOrder(1);
 
+		// Déclaration de plusieurs UserSport
+		UserSport us1 = new UserSport();
+		UserSport us2 = new UserSport();
+		UserSport us3 = new UserSport();
+		UserSport us4 = new UserSport();
+		UserSport us5 = new UserSport();
+		UserSport us6 = new UserSport();
+
+		// remplissage des UserSport
+		us1.setSport(foot);
+		us1.setUser(p1);
+		us1.setPreferenceOrder(2);
+		us2.setSport(kayak);
+		us2.setUser(p1);
+		us2.setPreferenceOrder(1);
+		us3.setSport(golf);
+		us3.setUser(p1);
+		us3.setPreferenceOrder(3);
+
+		us4.setSport(kayak);
+		us4.setUser(p2);
+		us4.setPreferenceOrder(1);
+
+		us4.setSport(foot);
+		us4.setUser(p3);
+		us4.setPreferenceOrder(1);
+
+		us6.setSport(tennis);
+		us6.setUser(p4);
+		us6.setPreferenceOrder(1);
+
+
 		// new SportPlaceAssociation(up2, us9);
 
 		userdao.save(p1);
@@ -218,6 +251,13 @@ public class DataBaseLoader {
 		upd.save(up8);
 		upd.save(up9);
 		upd.save(up10);
+
+		usd.save(us1);
+		usd.save(us2);
+		usd.save(us3);
+		usd.save(us4);
+		usd.save(us5);
+		usd.save(us6);
 	}
 
 }
