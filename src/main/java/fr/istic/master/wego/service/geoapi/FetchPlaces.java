20d94df8980aa288d3e52cc3d31ba1732aa6739b
Montuwy Angelique <angelique.montuwy@etudiant.univ-rennes1.fr>
2018-11-05 21:31:37 +0100

    fetching places from geoapi.gouv.fr

---
+++ src/main/java/fr/istic/master/wego/service/geoapi/FetchPlaces.java
@@ -0,0 +1,44 @@
+package fr.istic.master.wego.service.geoapi;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import fr.istic.master.wego.dao.PlaceDao;
+import fr.istic.master.wego.model.Place;
+
+@Component
+public class FetchPlaces {
+
+	@Autowired
+	PlaceDao placedao;
+	
+    public void fetchPlaces() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<Departement>> response = restTemplate.exchange("https://geo.api.gouv.fr/departements?fields=code",
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Departement>>() {
+                });
+        
+        List<Ville> villes = response.getBody().stream()
+                .map(d -> restTemplate.exchange("https://geo.api.gouv.fr/departements/" + d.getCode() + "/communes?fields=nom,codesPostaux", 
+                		HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Ville>>() {
+                }).getBody()).flatMap(x -> x.stream()).distinct().collect(Collectors.toList());
+
+        villes.forEach(v -> convertVille(v));
+    }
+    
+    public void convertVille(Ville ville) {
+    	ville.getCodesPostaux().forEach(p-> placedao.save(new Place(ville.getNom(),p)));
+    }
+}
